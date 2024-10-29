7c68afbfedc4fd09cec4a2deafb8c3f13962a2a0
mathildeperrissin@gmail.com <mathildeperrissin@gmail.com>
2024-10-29 14:21:21 +0000

    op margin 2

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -3,6 +3,6 @@ orders_id
 , date_date
 , round((margin+shipping_fee-logcost-ship_cost),2) as op_margin
 , quantity
-FROM {{ ref("int_orders_margin")}}
+FROM {{ ref("int_sales_margin")}}
  JOIN {{ ref("stg_raw__ship")}}
 USING (orders_id)
