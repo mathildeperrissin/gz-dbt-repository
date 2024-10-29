0c8f6f5ca124af9c296288f884b16882a4e2ee9c
mathildeperrissin@gmail.com <mathildeperrissin@gmail.com>
2024-10-29 15:03:27 +0000

    Model Requested by the Finance Team

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -2,9 +2,12 @@ SELECT
 date_date
 , count(orders_id) as nb_transactions
 , round(sum(revenue), 2) as revenue
-, round((sum(revenue)/count(orders_id)), 2) as average_basket
-, round(sum(op_margin), 2) as op_margin
-
+, ROUND(SUM(revenue)/COUNT(orders_id),2) AS average_basket
+, ROUND(SUM(op_margin),2) AS total_operational_margin
+, ROUND(SUM(purchase_cost),2) AS total_purchase_cost
+, ROUND(SUM(shipping_fee),2) AS total_shipping_fee
+, ROUND(SUM(logcost),2) AS total_logcost
+, COUNT(products_id) AS total_quantity_of_products_sold
 FROM {{ ref("int_orders_operational")}}
 GROUP BY date_date
 ORDER BY date_date desc
