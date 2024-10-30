320db34a2ded4e5f33c1bd662609cabefbcf6537
mathildeperrissin@gmail.com <mathildeperrissin@gmail.com>
2024-10-30 11:18:06 +0000

    add finance_campaigns_day.sql

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,19 @@
+SELECT 
+date_date
+, round((operational_margin-ads_cost),2) as ads_margin
+, average_basket
+, operational_margin
+, ads_cost
+, ads_impressions
+, ads_clicks
+, quantity
+, revenue
+, purchase_cost
+, margin
+, shipping_fee
+, logcost
+, ship_cost
+FROM {{ ref("int_campaigns_day")}}
+INNER JOIN {{ ref ("finance_days")}}
+USING (date_date)
+order by date_date desc
