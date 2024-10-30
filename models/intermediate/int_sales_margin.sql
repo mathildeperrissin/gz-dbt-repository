0f0813273fe091fcd32c4835058bfb46573ded88
mathildeperrissin@gmail.com <mathildeperrissin@gmail.com>
2024-10-30 13:51:23 +0000

    challenge 4

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,7 @@
 SELECT *
 , round(quantity*purchase_price,2) as purchase_cost
 , round((revenue-(quantity*purchase_price)),2) as margin
+, {{ margin_percent('revenue','quantity*purchase_price') }} AS margin_percent
 FROM {{ ref("stg_raw__sales")}}
     JOIN {{ ref("stg_raw__product")}}
 USING (products_id)
